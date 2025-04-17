ff8e37729084885431eb56fb926ae29d7f7207a1
Miguel <sci@maguilera.net>
2025-04-17 21:52:57 +0200

    correct TUR method

--- calculate_Fig1a.py
+++ calculate_Fig1a.py
@@ -94,9 +94,10 @@ def calc(N, rep):
 
         Pi=S_i.shape[1]/T
         # Estimate entropy production using various methods
-        sig_N1, sig_MTUR, theta1, Da = get_EP_Newton(S_i_t, i, Pi)
-        sigma_emp                    = exp_EP_spin_model(Da, J_t, i)
-        sig_N2, theta2               = get_EP_Newton2(S_i_t, theta1, Da, i, Pi)
+        sig_N1, theta1, Da          = get_EP_Newton(S_i_t, i)
+        sig_MTUR                    = get_EP_MTUR(S_i_t, Da, i)
+        sigma_emp                   = exp_EP_spin_model(Da, J_t, i)
+        sig_N2, theta2              = get_EP_Newton2(S_i_t, theta1, Da, i)
 
         # Aggregate results
         S_Emp += Pi*sigma_emp
