a1d5b812cdefece1683f3282602485f0c3660cbb
Artemy Kolchinsky <artemyk@gmail.com>
2025-04-18 10:46:12 +0200

    plot_example update

--- plot_example.sh
+++ plot_example.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ARGS="--patterns 10 --beta_max 5 --size 20 --num_beta 10  --BASE_DIR ~/MaxEntData2"
-#rm -rf ~/MaxEntData2
-#python generate_data_spin_model.py --seed 42 $ARGS
+rm -rf ~/MaxEntData2
+python generate_data_spin_model.py --seed 42 $ARGS
 python calculate_Fig1a_v2.py                    $ARGS
 
 
