17628f1ef1d8c0726de400484123846f3a8052e6
Artemy Kolchinsky <artemyk@gmail.com>
2025-05-07 19:57:10 +0200

    Update

--- results_neuropixels/fig2.sh
+++ results_neuropixels/fig2.sh
@@ -1,4 +1,5 @@
 echo "Remember to make sure ep_data/* is clean"
-# rm -rf ep_data/*
-python calculate_Fig2a.py --BASE_DIR ~/Downloads/neuropixels --lr 0.1 --max_sessions 1 --obs 2 --patience 30 --tol 0  --seed 100  --order sorted --sizes 50 100 150 200 250
-python display_Fig2a.py  --obs 2 --mode visual --order sorted
+#rm -rf ep_data/*
+python calculate_Fig2a.py --BASE_DIR ~/Downloads/neuropixels --max_sessions 1 --lr 0.1 --obs 2 --patience 30 --tol 0  --seed 100  --order sorted --sizes 50 100 150 200 250
+python display_Fig2a.py  --obs 2 --order sorted
+
