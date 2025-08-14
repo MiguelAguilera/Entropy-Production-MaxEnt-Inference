045a3a6a58c8ae95c9c615020bbe01121bd9bd93
Artemy Kolchinsky <artemyk@gmail.com>
2025-08-14 12:58:25 +0200

    Update NEEP gradient

--- other/NEEP.py
+++ other/NEEP.py
@@ -13,3 +13,8 @@ class DatasetNEEP(observables.Dataset):
         Z                   = norm_const * torch.exp( th_g_max ) 
         return float( theta @ self.g_mean - Z + 1 )
 
+    def get_gradient(self, theta):
+        theta = numpy_to_torch(theta)
+        th_g_max, norm_const, _ = self._get_tilted_values(theta)
+        Z                   = norm_const * torch.exp( th_g_max ) 
+        return self.g_mean - Z*self.get_tilted_mean(theta)  # Gradient of the objective function
