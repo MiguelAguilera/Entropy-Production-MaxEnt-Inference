32777e9d8016897b01018aae77392c16a572bfe9
Artemy Kolchinsky <artemyk@gmail.com>
2025-08-15 14:09:15 +0200

    Update

--- other/neep-counter-example.py
+++ other/neep-counter-example.py
@@ -7,24 +7,24 @@ def kl(p,q): # KL divergence
 
 
 
-
-def get_P(r):
+def get_P(a,b):
     # construct a 3x3 steady-state joint probability matrix
     # Here we consider a uniform unicyclic system, with backward rate r
-    d = .1 # probability of staying on same state
-    o = 1-r-d
-    assert(o >= 0)
+    d = 1-a-b # probability of staying on same state
+    assert(d >= 0)
     T = np.array([
-        [d, o, r],
-        [r, d, o],
-        [o, r, d],
+        [d, a, b],
+        [b, d, a],
+        [a, b, d],
     ])
+    #print(T[1,0],a,b)
+    #sadf
     pi = np.ones(3)/3 # uniform steady state
-    assert(np.allclose(T.sum(axis=0), 1.0))  
-    assert(np.allclose(pi,T@pi))
+    assert(np.allclose(T.sum(axis=1), 1.0))  
+    assert(np.allclose(pi,pi@T))
 
     # Forward and reverse probability distributions
-    P = T * pi[None,:]
+    P = T * pi[:,None]
     R = P.T
 
     return P, R
@@ -38,7 +38,7 @@ def f(P, R, obs, method): # Optimize variational expression
     else:           # NEEP
         objective = cp.Maximize(x * expectation - cp.sum(cp.multiply(R, cp.exp(x * obs))) + 1)
     prob = cp.Problem(objective)
-    return prob.solve(solver=cp.MOSEK)
+    return prob.solve(solver=cp.CLARABEL)
 
 # Create observables
 M =np.array([[ 0, 1, 0],
@@ -55,11 +55,14 @@ vals_ep   = []
 if True:  # sweep across driving strengths (asymmetry parameters)
     SEMILOG=False
     XLABEL = r'Driving strength $\lambda$'
-    l_vals = np.logspace(-20,0,500)/4
-    l_vals = np.arange(0, 20)
+    #l_vals = np.logspace(-20,0,100)
+    l_vals = np.arange(2, 30)
     for l in l_vals:
-        P, R = get_P(r=np.exp(-l)/2)
-        obs = A + S * 2
+        r=np.exp(-l)
+        a=1/(1+r)/2
+        b=r/(1+r)/2
+        P, R = get_P(a,b)
+        obs = A + 1.5*S
         vals_ours.append( f(P, R, obs, method=0) )
         vals_NEEP.append( f(P, R, obs, method=1) )
         vals_ep.append( kl(P,R) )
@@ -84,14 +87,31 @@ vals_NEEP = np.array(vals_NEEP)
 vals_ep   = np.array(vals_ep)
 
 import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style='white', font_scale=1.2)
+plt.rc('text', usetex=True)
+plt.rc('font', size=14, family='serif', serif=['latin modern roman'])
+plt.rc('legend', fontsize=12)
+#plt.rc('text.latex', preamble=r'\usepackage{amsmath,bm}')
 
-plt.plot(l_vals, vals_ep, label='True', c='k')
 
-plt.plot(l_vals, vals_ours, label='Ours')
-plt.plot(l_vals, vals_NEEP, label='NEEP')
+plt.figure(figsize=(5,4), layout='constrained')
+var_bound = np.log(27)/2-1
+var_bound = 5*np.log(5)/2-2
+
+plt.plot(l_vals, vals_ep, label=r'$\Sigma$', c='k')
+
+plt.plot(l_vals, vals_ours, label=r'$\Sigma_g$')
 if SEMILOG:
     plt.semilogx()
+plt.plot(l_vals, vals_NEEP, label=r'$\Sigma_g^\prime$')
+plt.plot(l_vals, l_vals*0+var_bound, c='k',ls=':', #ls='none',marker='o', lw=1,markersize=3,
+         label=r'$\Sigma_g^\prime \;\;(\lambda \to \infty)$')
 plt.legend()
 plt.xlabel(XLABEL)
+plt.xlim(l_vals.min(), l_vals.max())
 plt.ylabel('Entropy production')
-plt.show()
+plt.savefig('vs-neep.pdf')
+
+import os
+os.system('pdfcrop vs-neep')
